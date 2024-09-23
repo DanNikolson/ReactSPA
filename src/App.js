a872cf0e945cce6e2c39e620dd16e8f3f6cef530
Dan Nikolson <Kuznetsov556677@yandex.ru>
2024-09-23 09:15:00 +0300

    PostItem component, styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
+import "./styles/App.css";
+import PostItem from "./components/PostItem";
 function App() {
   return (
     <div className="App">
-      <ClassCounter />
+      <PostItem />
     </div>
   );
 }
