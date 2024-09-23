ec856ce0046c365439b5770064ee877c642e5a66
Dan Nikolson <Kuznetsov556677@yandex.ru>
2024-09-23 09:37:58 +0300

    PostItem,PostList components

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 import "./styles/App.css";
 import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
 function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: "javascript1", body: "Description" },
+    { id: 2, title: "javascript2", body: "Description" },
+    { id: 3, title: "javascript3", body: "Description" },
+  ]);
   return (
     <div className="App">
-      <PostItem />
+      <PostList posts={posts} title="Посты про JS" />
     </div>
   );
 }
--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
-const PostItem = () => {
+const PostItem = (props) => {
+  console.log(props);
   return (
     <div className="post">
       <div className="post__content">
-        <strong>1. javascript</strong>
-        <div> javascript - язык программирования</div>
+        <strong>
+          {props.post.id}. {props.post.title}
+        </strong>
+        <div> {props.post.body}</div>
       </div>
       <div className="post__btns">
         <button>Удалить</button>
