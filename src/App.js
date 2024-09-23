27460ed16ae05023f4800820fe7a644f32077335
Dan Nikolson <Kuznetsov556677@yandex.ru>
2024-09-23 18:41:33 +0300

    Deleted useless button

--- src/App.js
+++ src/App.js
@@ -50,7 +50,6 @@ function App() {
 
   return (
     <div className="App">
-      <MyButton onClick={fetchPosts}>Get Posts</MyButton>
       <MyButton style={{ marginTop: 30 }} onClick={() => setModal(true)}>
         Создать пост
       </MyButton>
