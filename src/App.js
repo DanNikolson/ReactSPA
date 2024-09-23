693f979b720d143763ded3e93f5a3b0c5fd6a758
Dan Nikolson <Kuznetsov556677@yandex.ru>
2024-09-23 09:05:13 +0300

    ClassCounter class component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <ClassCounter />
     </div>
   );
 }
