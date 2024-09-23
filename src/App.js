16dc97983c8dbefd8357b6240da6802ee5e98bfe
Dan Nikolson <Kuznetsov556677@yandex.ru>
2024-09-23 16:30:17 +0300

    Loader - for custom loading animation, Loader styles

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import { usePosts } from "./hooks/usePosts";
 import PostService from "./API/PostService";
+import Loader from "./components/UI/Loader/Loader";
 function App() {
   const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({ sort: "", query: "" });
@@ -47,7 +48,11 @@ function App() {
       <hr style={{ margin: "15px 0" }} />
       <PostFilter filter={filter} setFilter={setFilter} />
       {isPostsLoading ? (
-        <h1>Идёт загрузка...</h1>
+        <div
+          style={{ display: "flex", justifyContent: "center", marginTop: 50 }}
+        >
+          <Loader />
+        </div>
       ) : (
         <PostList
           remove={removePost}
