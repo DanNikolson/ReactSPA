a45d62937dff6b9af78aafebcea698b39892b590
Dan Nikolson <Kuznetsov556677@yandex.ru>
2024-09-23 17:50:14 +0300

    bug fix

--- src/components/UI/pagination/Pagination.jsx
+++ src/components/UI/pagination/Pagination.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { usePagination } from "../../../hooks/usePagination";
 
-const Pagination = (totalPages, page, changePage) => {
+const Pagination = ({ totalPages, page, changePage }) => {
   let pagesArray = usePagination(totalPages);
   return (
     <div className="page__wrapper">
