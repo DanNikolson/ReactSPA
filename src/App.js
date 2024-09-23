c4e2ff50d3a3aadf4a406ab7acb672ea501a4c1e
Dan Nikolson <Kuznetsov556677@yandex.ru>
2024-09-23 13:00:43 +0300

    MyModal - closing my clicking outside of the fields

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
+import MyButton from "./components/UI/button/MyButton";
 function App() {
   const [posts, setPosts] = useState([
     { id: 1, title: "a1", body: "c" },
@@ -11,6 +12,7 @@ function App() {
     { id: 3, title: "b3", body: "b" },
   ]);
   const [filter, setFilter] = useState({ sort: "", query: "" });
+  const [modal, setModal] = useState(false);
 
   const sortedPosts = useMemo(() => {
     if (filter.sort) {
@@ -28,6 +30,7 @@ function App() {
   }, [filter.query, sortedPosts]);
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
+    setModal(false);
   };
 
   const removePost = (post) => {
@@ -36,7 +39,10 @@ function App() {
 
   return (
     <div className="App">
-      <MyModal visible={true}>
+      <MyButton style={{ marginTop: 30 }} onClick={() => setModal(true)}>
+        Создать пост
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
         <PostForm create={createPost} />
       </MyModal>
 
